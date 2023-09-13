3df54bc3375fff9229b9e496fb039363983173a1
ErickMeucci <144945118+ErickMeucci@users.noreply.github.com>
2023-09-13 20:46:08 -0300

    Prova da faculdade impact

---
+++ Exercicio 1.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = x % 2
+if (y == 0):
+    i = x - 1
+    p = x + 2
+else:
+    i = x - 2
+    p = x + 1
+
+print(i,p)
---
+++ Exercicio 2.py
@@ -0,0 +1,12 @@
+n1 = float(input())
+n2 = float(input())
+
+m = (n1+n2)/2
+s = (n1+10)/2
+
+if (m >= 6):
+    print('aprovado')
+elif (s < 6):
+    print('reprovado')
+else:
+    print('talvez com a sub')
